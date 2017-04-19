86304c520edb84e60f6c5fcf6247dd4a54efac4d
William Munslow <will@subterrane.com>
2017-04-19 14:34:11 -0700

    start

---
+++ js/calendar.js
@@ -0,0 +1,15 @@
+  document.addEventListener("DOMContentLoaded", main);
+
+  function main(event) {
+      var calendarEl = document.getElementsByTagName('calendar')[0];
+
+
+
+
+      console.log(calendarEl);
+      calendarEl.innerHTML = 'calendar';
+
+      var today = new Date();
+
+
+  }
