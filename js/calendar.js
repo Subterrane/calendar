15e1e4d8b75091e2f2ab6705b37146fbd545b982
William Munslow <will@subterrane.com>
2017-04-26 15:16:58 -0700

    add custom 'calendarClicked' event, multiple calendar support, etc

--- js/calendar.js
+++ js/calendar.js
@@ -1,22 +1,41 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function (event) {
-    var calendarEl = document.getElementsByTagName('calendar')[0];
-    var now = new Date();
-    var y = parseInt(calendarEl.getAttribute('year')) || now.getFullYear();
-    var m = parseInt(calendarEl.getAttribute('month')) || now.getMonth();
-    var d = parseInt(calendarEl.getAttribute('day')) || now.getDate();
-    var time = new Date(y, --m, d);
-    var month = time.getMonth();
-    var week = 1;
-    var day = 1;
-    var html = "<month number=\"" + (month + 1) + "\"><week number=\"" + week + "\">";
-    do {
-        time.setDate(day);
-        if (time.getDay() % 7 === 0)
-            html += "</week><week number=\"" + ++week + "\">";
-        html += "<day number=\"" + day + "\"></day>";
-        day++;
-    } while (month === time.getMonth());
-    html += '</week></month>';
-    calendarEl.innerHTML = html;
+    var calendars = document.getElementsByTagName('calendar');
+    for (var ix = 0; ix < calendars.length; ix++) {
+        var calendarEl = calendars[ix];
+        var now = new Date();
+        var y = parseInt(calendarEl.getAttribute('year')) || now.getFullYear();
+        var m = parseInt(calendarEl.getAttribute('month')) || now.getMonth();
+        var d = parseInt(calendarEl.getAttribute('day')) || now.getDate();
+        var time = new Date(y, m - 1, d);
+        var month = time.getMonth();
+        var week = 1;
+        var day = 1;
+        var html = "<year number=\"" + y + "\"><month number=\"" + m + "\"><week number=\"" + week + "\">";
+        do {
+            time.setDate(day);
+            if (time.getDay() % 7 === 0)
+                html += "</week><week number=\"" + ++week + "\">";
+            html += "<day number=\"" + day + "\"></day>";
+            day++;
+        } while (month === time.getMonth());
+        html += '</week></month>';
+        calendarEl.innerHTML = html;
+        calendarEl.querySelector('day[number="' + d + '"]').classList.add('today');
+    }
+    var days = document.querySelectorAll('day');
+    for (var ix = 0; ix < days.length; ix++) {
+        days[ix].addEventListener('click', dayClicked);
+    }
 });
+function dayClicked() {
+    var el = this;
+    var data = {
+        year: el.parentElement.parentElement.parentElement.getAttribute('number'),
+        month: el.parentElement.parentElement.getAttribute('number'),
+        week: el.parentElement.getAttribute('number'),
+        day: el.getAttribute('number')
+    };
+    var event = new CustomEvent('calendarClicked', { 'detail': data });
+    document.dispatchEvent(event);
+}
--- js/calendar.ts
+++ js/calendar.ts
@@ -1,24 +1,47 @@
 document.addEventListener("DOMContentLoaded", event => {
-    let calendarEl = document.getElementsByTagName('calendar')[0];
-    let now = new Date();
+    let calendars = document.getElementsByTagName('calendar');
 
-    let y = parseInt(calendarEl.getAttribute('year')) || now.getFullYear();
-    let m = parseInt(calendarEl.getAttribute('month')) || now.getMonth();
-    let d = parseInt(calendarEl.getAttribute('day')) || now.getDate();
+    for (let ix = 0; ix < calendars.length; ix++) {
+        let calendarEl = calendars[ix];
+        let now = new Date();
 
-    let time = new Date(y, --m, d);
-    let month = time.getMonth();
-    let week = 1;
-    let day = 1;
-    let html = `<month number="${month + 1}"><week number="${week}">`;
+        let y = parseInt(calendarEl.getAttribute('year')) || now.getFullYear();
+        let m = parseInt(calendarEl.getAttribute('month')) || now.getMonth();
+        let d = parseInt(calendarEl.getAttribute('day')) || now.getDate();
 
-    do {
-        time.setDate(day);
-        if(time.getDay() % 7 === 0) html += `</week><week number="${++week}">`;
-        html += `<day number="${day}"></day>`;
-        day++;
-    } while(month === time.getMonth());
+        let time = new Date(y, m - 1, d);
+        let month = time.getMonth();
+        let week = 1;
+        let day = 1;
+        let html = `<year number="${y}"><month number="${m}"><week number="${week}">`;
 
-    html += '</week></month>'
-    calendarEl.innerHTML = html;
+        do {
+            time.setDate(day);
+            if (time.getDay() % 7 === 0) html += `</week><week number="${++week}">`;
+            html += `<day number="${day}"></day>`;
+            day++;
+        } while (month === time.getMonth());
+
+        html += '</week></month>'
+        calendarEl.innerHTML = html;
+
+        calendarEl.querySelector('day[number="' + d + '"]').classList.add('today');
+    }
+
+    let days = document.querySelectorAll('day');
+    for(let ix = 0; ix < days.length; ix++) {
+        days[ix].addEventListener('click', dayClicked);
+    }
 });
+
+function dayClicked() {
+    let el = this;
+    let data = {
+        year: el.parentElement.parentElement.parentElement.getAttribute('number'),
+        month: el.parentElement.parentElement.getAttribute('number'),
+        week: el.parentElement.getAttribute('number'),
+        day: el.getAttribute('number')
+    };
+    var event = new CustomEvent('calendarClicked', {'detail': data});
+    document.dispatchEvent(event);
+}
